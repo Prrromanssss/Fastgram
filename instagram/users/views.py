c7fc63d0f6ac0bf8c5d9f8305928b3738c644d1a
Vsevolod <steep273@gmail.com>
2022-12-15 18:41:19 +0300

    Modified_profile_page

--- instagram/users/views.py
+++ instagram/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
+from django.shortcuts import render, redirect
 
 from users.forms import CustomUserCreationForm, CustomUserChangeForm
 from users.models import CustomUser
@@ -29,7 +30,23 @@ class ProfileChangeView(FormView):
     form_class = CustomUserChangeForm
     success_url = reverse_lazy('users:profile')
 
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        return super().form_valid(form)
+    def get(self, request):
+        form = self.form_class(
+            initial=self.initial,
+            instance=request.user,
+        )
+        context = {'form': form, 'user': request.user}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(
+            request.POST or None,
+            instance=request.user,
+        )
+        if form.is_valid():
+            self.model.objects.filter(id=request.user.id).update(
+                **form.cleaned_data,
+            )
+            return redirect(self.get_success_url())
+        context = {'form': form, 'user': request.user}
+        return render(request, self.template_name, context)
