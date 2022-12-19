1c02cd208ddf08170114ec28bd07af78a79c5696
Sourr_cream <roman_boyko.48@mail.ru>
2022-12-20 01:22:07 +0300

    change name of the project

--- fastgram/fastgram/asgi.py
+++ fastgram/fastgram/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for instagram project.
+ASGI config for fastgram project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- fastgram/fastgram/wsgi.py
+++ fastgram/fastgram/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for instagram project.
+WSGI config for fastgram project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
