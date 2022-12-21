dde1281bff80cd5c4c0acd8a539133d7bd9270c2
Vsevolod <steep273@gmail.com>
2022-12-21 19:50:06 +0300

    updated likes

--- instagram/response/models.py
+++ instagram/response/models.py
@@ -36,12 +36,12 @@ class Response(NameBaseModel):
         blank=True,
     )
 
-    def get_likes(self):
-        return self.likes.values('id')
-
     class Meta:
         verbose_name = 'отзыв'
         verbose_name_plural = 'отзывы'
+
+    def get_likes(self):
+        return self.likes.only('id')
 
 
 class Delivery(NameBaseModel, IsPublishedBaseModel):
@@ -67,3 +67,20 @@ class MainImage(ImageBaseModel):
     class Meta:
         verbose_name = 'изображение'
         verbose_name_plural = 'изображения'
+
+
+class Comments(models.Model):
+    text = RichTextField(
+        'текст',
+        help_text='Напишите свой комментарий'
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        verbose_name='пользователь',
+        on_delete=models.CASCADE,
+    )
+    response = models.ForeignKey(
+        Response,
+        verbose_name='отзыв',
+        on_delete=models.CASCADE
+    )
