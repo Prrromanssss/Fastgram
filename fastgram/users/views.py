f39b207c8d256c4acfade824b689b2b6e91957b6
Sourr_cream <roman_boyko.48@mail.ru>
2022-12-24 00:31:31 +0300

    resolve cbv in users

--- fastgram/users/views.py
+++ fastgram/users/views.py
@@ -27,20 +27,22 @@ class ProfileView(LoginRequiredMixin, FormView):
     form_class = CustomUserChangeForm
     success_url = reverse_lazy('users:profile')
 
-    def post(self, request):
-        form = self.form_class(
-            request.POST or None,
-            request.FILES,
-            instance=request.user,
-            )
-        if form.is_valid():
-            old_image = CustomUser.objects.get(id=request.user.id).image
-            if old_image:
-                image_path = old_image.path
-                if os.path.exists(image_path):
-                    os.remove(image_path)
-            form.save()
-        return super().post(self, request)
+    def get_form(self):
+        return self.form_class(
+               self.request.POST or None,
+               self.request.FILES,
+               instance=self.request.user,
+                )
+
+    def form_valid(self, form):
+        old_image = CustomUser.objects.get(id=self.request.user.id).image
+        if old_image:
+            image_path = old_image.path
+            if os.path.exists(image_path):
+                os.remove(image_path)
+        form.save()
+
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
